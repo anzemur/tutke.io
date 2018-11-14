9652ac4bdf83dbfee154ff244399e16bcc4667a3
Anze Mur <am3158@student.uni-lj.si>
2018-11-15 00:08:04 +0100

    Minor changes

--- app/app.js
+++ app/app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
+var viewsRouter = require('./app_server/routes/viewsRoutes');
 
 var app = express();
 
@@ -19,11 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-
-
+app.use('/', viewsRouter);
 
 
 /* Catch 404 and forward to error handler */
