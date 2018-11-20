31e9e84db117e2c9ca91d9cce283568a68cf7a47
Anze Mur <am3158@student.uni-lj.si>
2018-11-20 23:01:45 +0100

    Added user model

---
+++ app/app_api/models/users.js
@@ -0,0 +1,25 @@
+'use strict';
+var mongoose = require('mongoose');
+
+var commentSchema = new mongoose.Schema({
+  author: {type: String, required: true},
+  rating: {type: Number, required: true, min: 0, max: 5},
+  commentText: {type: String, required: true},
+  createdAt: {type: Date, "default": Date.now}
+});
+
+var userSchema = new mongoose.Schema({
+  username: {type: String, required: true, unique: true, dropDups: true},
+  password: {type: String, required: true},
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  email: {type: String, unique: true, dropDups: true, required: true},
+  teachingInstitution: String,
+  educationLevel: String,
+  fieldOfEducation: String,
+  rating: {type: Number, "default": 0, min: 0, max: 5}, 
+  role: {type: String, required: true},
+  comments: [commentSchema]
+});
+
+mongoose.model('User', userSchema, 'Users');
