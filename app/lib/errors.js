65e14efb2daf3a1ce36e82cf632af74af6370c44
Anze Mur <am3158@student.uni-lj.si>
2018-11-23 00:24:43 +0100

    Added error mapping

---
+++ app/lib/errors.js
@@ -0,0 +1,8 @@
+module.exports = {
+  NotFound: "Not found.",
+  BadRequest: "Bad request, missing parameter: ",
+  Unauthorized:  "You are not authorized to do this action.",
+  Forbidden: "You don't have the permission to do this action.",
+  ServerError: "Internal server error",
+  Conflict: "Model with unique attributes already exists. Conflicts: "
+}
