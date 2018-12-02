5edd7f8596518bcb8eda53cd797c2ee9f901bb9f
Anze Mur <am3158@student.uni-lj.si>
2018-12-02 15:00:08 +0100

    Populated users profile

--- app/app_server/controllers/user-profile.js
+++ app/app_server/controllers/user-profile.js
@@ -4,6 +4,50 @@ var apiParams = helperFunctions.getApiParams();
 
 
 /* GET user profile page */
-module.exports.userProfile = function(req, res) {
-  res.render('user-preview', { title: 'User' });
+module.exports.userProfile = async (req, res) => {
+
+  /* Simulate logged in user instead of using local storage with JWT  */
+  var user;
+  if(loggedInUser) {
+    user = await getUser(loggedInUser);
+    console.log(user);
+  }
+
+  var currentUserError;
+  var currentUser;
+  if(req.params.userId) {
+    currentUser = await getUser(req.params.userId);
+    if(currentUser.error) {
+      currentUserError = currentUser.error.message ? currentUser.error.message : currentUser.error;
+      currentUser = null;
+    }
+  } else {
+    currentUserError = 'Couldn\'t load user info.';
+  }
+
+  res.render('user-preview', { 
+    title: currentUser ? currentUser.username : 'User',
+    currentUser: currentUser,
+    user: user,
+    currentUserError: currentUserError
+  });
 };
+
+
+async function getUser(userId) {
+  var path = '/users/' + userId;
+  var options = {
+    url: apiParams + path,
+    method: 'GET',
+    json: {},
+    qs: {
+      populate: true
+    }
+  };
+
+  try {
+    return await rp(options).promise();
+  } catch (error) {
+    return error;
+  }
+}
