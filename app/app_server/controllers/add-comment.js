9caca67d236fb5a122cffeb2868c9278d80e2b53
Lovro Kalan <lovro.kalan1@gmail.com>
2018-12-08 19:50:00 +0100

    Added edit comment controller

--- app/app_server/controllers/add-comment.js
+++ app/app_server/controllers/add-comment.js
@@ -63,6 +63,88 @@ module.exports.addComment = async(req, res) => {
  });
 }
 
+/* GET edit review page */
+module.exports.editCommentPage = async (req, res) => {
+  var successMsg;
+  var errorMsg;
+
+  /* Simulate logged in user instead of using local storage with JWT  */
+  var user;
+  if(loggedInUser) {
+    user = await getUser(loggedInUser);
+  }
+
+  var comment;
+  if(req.params.commentId) {
+    comment = await getComment(req.params.userId ,req.params.commentId);
+  } else {
+    errorMsg = 'commentId is missing or undefined';
+  }
+
+  if(!user) {
+    res.render('log-in', { 
+      title: 'Log In',
+      error: 'Please log in to see additonal information.'
+    });
+  } else {
+    res.render('edit-review', { 
+      title: 'Edit review',
+      user: user,
+      currentUser: req.params.userId,
+      commentContent: comment,
+      errorMsg: errorMsg,
+      successMsg: successMsg
+    });
+  }
+};
+
+/* POST edit review  */
+module.exports.editCommentReq = async (req, res) => {
+  var successMsg;
+  var errorMsg;
+
+  /* Simulate logged in user instead of using local storage with JWT  */
+  var user;
+  if(loggedInUser) {
+    user = await getUser(loggedInUser);
+  }
+
+  /** Add comment to the db. */
+  if(user && user._id && req.body.rating && req.body.commentText && req.params.userId) {
+    req.body.author = user._id;
+    var comment = await editComment(req.body, req.params.userId, req.params.commentId);
+    if(comment.error) {
+      errorMsg = comment.error.message ? comment.error.message : comment.error;
+    } else {
+      successMsg = 'Comment successfully edited.'
+    }
+
+  } else {
+    errorMsg = 'Please enter all of the required data!';
+  }
+
+  var currentUser;
+  if(req.params.userId) {
+    currentUser = await getUser(req.params.userId);
+    if(currentUser.error) {
+      errorMsg = currentUser.error.message ? currentUser.error.message : currentUser.error;
+      currentUser = null;
+    }
+  } else {
+    errorMsg = 'Couldn\'t load user info.';
+  }
+
+ res.render('user-preview', { 
+   title: currentUser ? currentUser.username : 'User',
+   user: user,
+   currentUser: currentUser,
+   errorMsg: errorMsg,
+   successMsg: successMsg
+
+ });
+
+};
+  
 
 async function getUser(userId) {
   var path = '/users/' + userId;
@@ -82,11 +164,42 @@ async function getUser(userId) {
   }
 }
 
+async function getComment(userId, commentId) {
+  var path = '/users/' + userId + '/comments/' + commentId;
+  var options = {
+    url: apiParams + path,
+    method: 'GET',
+    json: {},
+  };
+
+  try {
+    return await rp(options).promise();
+  } catch (error) {
+    return error;
+  }
+}
+
 async function addCommentToUser(body, userId) {
   var path = '/users/' + userId + '/comments'
   var options = {
     url: apiParams + path,
     method: 'POST',
+    json: true,
+    body: body
+  };
+
+  try {
+    return await rp(options).promise();
+  } catch (error) {
+    return error;
+  }
+}
+
+async function editComment(body, userId, commentId) {
+  var path = '/users/' + userId + '/comments/' + commentId
+  var options = {
+    url: apiParams + path,
+    method: 'PUT',
     json: true,
     body: body
   };
