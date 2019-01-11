05112b5620afdf7fa85b4658011398f9f52b5591
Lovro Kalan <lovro.kalan1@gmail.com>
2019-01-11 17:38:22 +0100

    connected user data to edit popup form

--- app/app_client/edit-user-pop-up/edit-user-pop-up.controller.js
+++ app/app_client/edit-user-pop-up/edit-user-pop-up.controller.js
@@ -2,6 +2,16 @@
   function editUserCtrl($uibModalInstance, userData) {
     var vm = this;
 
+    vm.formData = {
+      username: "",
+      firstName: "",
+      lastName: "",
+      email: "",
+      teachingInstitution: "",
+      educationLevel: "",
+      fieldOfEducation: ""
+    }
+
     vm.userToEdit = userData;
 
     vm.editUserPopUp = {
@@ -9,6 +19,17 @@
         $uibModalInstance.close();
       }
     };
+
+    if (vm.userToEdit) {
+      vm.formData.firstName = vm.userToEdit.firstName;
+      vm.formData.lastName = vm.userToEdit.lastName;
+      vm.formData.username = vm.userToEdit.username;
+      vm.formData.email = vm.userToEdit.email;
+      vm.formData.teachingInstitution = vm.userToEdit.teachingInstitution;
+      vm.formData.educationLevel = vm.userToEdit.educationLevel;
+      vm.formData.fieldOfEducation = vm.userToEdit.fieldOfEducation;
+    }
+
   }
   editUserCtrl.$inject = ['$uibModalInstance', 'userData'];
 
